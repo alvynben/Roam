5af2a668202d0828fda3cffb22f593f40817995c
alvynben <alvynben@gmail.com>
2024-05-20 12:35:26 +0800

    Initial Commit

---
+++ Roam_MVP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Roam_MVP
+//
+//  Created by Alvin Abraham on 20/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Roam_MVP/Roam_MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Roam_MVPApp.swift
+//  Roam_MVP
+//
+//  Created by Alvin Abraham on 20/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Roam_MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
